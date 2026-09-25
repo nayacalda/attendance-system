8066d2236c0002194d6ad12921a6f69aabbbe301
ITC <ayancalda8@gmail.com>
2026-09-25 15:24:43 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,2 @@
+// Paste your deployed Google Apps Script Web App URL here
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw_a1sGWqcJUkkuvQveuYOZxrNGv8_aiqTZ_xzAvFrS8a0MWHy4xU_ckQmxtwzULVovfA/exec';
